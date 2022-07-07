b350663d77ee722b7f00b7cec5b41f1c28098c58
Carlos Franzreb <carlosfranzreb@gmail.com>
2022-07-07 09:39:18 +0200

    Add an example

---
+++ example.py
@@ -0,0 +1,30 @@
+""" Example of how to use the text processor.
+
+Installation steps:
+
+1. Install requirements: pip install -r requirements.txt
+2. Install spacy model: python -m spacy download en_core_web_sm
+3. Open a python shell and execute the following commands:
+    a. import nltk
+    b. nltk.download("english")
+    c. nltk.download("omw-1.4")
+"""
+
+
+from flair.tokenization import SpacyTokenizer
+from flair.models import SequenceTagger
+from nltk.stem import WordNetLemmatizer
+
+from processor import DataProcessor
+
+
+if __name__ == "__main__":
+    tokenizer = SpacyTokenizer("en_core_web_sm")
+    lemmatizer = WordNetLemmatizer()
+    tagger = SequenceTagger.load("upos-fast")
+    processor = DataProcessor(tokenizer, tagger, lemmatizer)
+    text = "This text is going to be processed; in different ways!"
+    print(processor.tokenize(text, filter=False))
+    print(processor.tokenize(text))
+    print(processor.lemmatize(text, filter=False))
+    print(processor.lemmatize(text))
